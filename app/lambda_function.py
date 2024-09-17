19814b4c6c11b8c0b8451c95d115a7229c9676c8
Henrique de Paula <henrique.depaula@hotmail.com>
2024-09-16 22:40:34 -0300

    .

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,7 +1,6 @@
 import json
 import boto3
 from lambda_function_log import Logger
-import boto3
 
 logger = Logger()
 
@@ -9,7 +8,7 @@ nome_tabela = "eventos_lambda"
 dynamo = boto3.resource('dynamodb').Table(nome_tabela)
 
 def create(request):
-    return dynamo.put_item(request)
+    return dynamo.put_item(json.dumps(request))
 
 def read(request):
     return dynamo.scan()
