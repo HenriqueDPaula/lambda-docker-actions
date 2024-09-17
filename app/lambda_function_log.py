c0da3ffab932dcd4124e2873b4a2cf25bb6a0c86
Henrique de Paula <henrique.depaula@hotmail.com>
2024-09-16 21:43:57 -0300

    resolve conflicts

--- app/lambda_function_log.py
+++ app/lambda_function_log.py
@@ -1,7 +1,10 @@
+import os
+
 class Logger:
     def info(self, message):
         """Log de informação"""
         print(f"[INFO] {message}")
+        print(f"[INFO] Ambiente = {os.getenv('ENV')}")
     
     def error(self, message):
         """Log de erro"""
